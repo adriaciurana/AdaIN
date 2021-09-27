6923aa8c84545975822ad9cf38243b7ba76ecf02
Adrià Ciurana <info@dreamlearning.ai>
2021-09-27 11:37:57 +0200

    Seems that this loss improve a little bit.

--- losses.py
+++ losses.py
@@ -1,8 +1,20 @@
+import torch
 from torch.nn import functional as F
 from utils import compute_mu_sigma
 
+# def content_loss(feat_g_t, feat_content):
+#     w = torch.abs(feat_content) + 0.1
+#     return (w * (feat_g_t - feat_content) ** 2).mean()
+
 def content_loss(feat_g_t, feat_content):
-    return F.mse_loss(feat_g_t, feat_content)
+    fc_dx = torch.diff(feat_content, dim=1)
+    fc_dy = torch.diff(feat_content, dim=2)
+    gt_dx = torch.diff(feat_content, dim=1)
+    gt_dy = torch.diff(feat_content, dim=2)
+
+    color_loss = 0.5 * F.mse_loss(feat_g_t, feat_content)
+    diff_loss = 0.5 * (F.mse_loss(fc_dx, gt_dx) + F.mse_loss(fc_dy, gt_dy))
+    return color_loss + diff_loss
 
 
 def style_loss(phi_list_g_t, phi_list_style):
